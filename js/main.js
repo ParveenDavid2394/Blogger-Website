f7c307523f92729eddf5e0b8addab221e1a4dce3
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-09-02 23:12:52 +0800

    used Jquery library to perform click event on toggle

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    // click event on toggle menu
+    $toggleCollapse.click(function (e) { 
+        e.preventDefault();
+
+        $nav.toggleClass('collapse');
+
+        
+    });
+});
